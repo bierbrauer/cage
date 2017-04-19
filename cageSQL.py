fc449f96b8feb2e27db5788b37b694cc05ae23f3
David Bierbrauer <davidbierbrauer.db@gmail.com>
2017-04-19 23:27:28 +0200

    - moved findArduino to cageSQL

--- cageSQL.py
+++ cageSQL.py
@@ -1,7 +1,19 @@
-#!/usr/bin/python
-import serial
+#!/bin/bash/python
+import serial.tools.list_ports
+import sys
+import atexit
+import platform
 import time
 import MySQLdb
+
+# Arduino detection information
+ARUDINO_SERIAL_NUMBER_LENGTH = 20
+ARDUINO_SERIAL_NUMBER_IDENTIFIER = "SER="
+ARDUINO_UNO_PID = "VID:PID=2341:0043"
+SERIAL_PORT_PATH = "/dev"
+
+arduinoStatus = {}
+connectedArduinos = {}
 
 # DB Data Indeces
 CAGE = 0
@@ -26,6 +38,41 @@ def parseInput(data):
 
 def getCurrentMillis():
     return int(time.time() * 1000)
+
+def saveArduinoEvents(serialNumber, serialPort, event):
+    timestamp = getCurrentMillis()
+    
+    db = MySQLdb.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+        db="cage")
+
+    cur = db.cursor()
+
+    try:
+        cur.execute("""
+            INSERT INTO arduino_events
+            (`serialNumber`, `serialPort`, `timestamp`, `event`)
+            VALUES(%s,%s,%s,%s)""", (
+            serialNumber, serialPort, timestamp, event
+        ))
+        db.commit()
+    except MySQLdb.Error, e:
+        try:
+            print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+            return None
+        except IndexError:
+            print "MySQL Error: %s" % str(e)
+            return None
+    except TypeError, e:
+        print("MySQL Error: TypeError: %s" % str(e))
+        return None
+    except ValueError, e:
+        print("MySQL Error: ValueError: %s" % str(e))
+        return None  
+
+    db.close()
 
 def saveToDatabase(data):
     print("Parsing data...")
@@ -71,16 +118,89 @@ def saveToDatabase(data):
         return None  
 
     print("Close database connection")
-    db.close() 
+    db.close()
 
-# Establish connection to Arduino
-ser = serial.Serial('/dev/ttyACM0',9600)
-ser.open()
-time.sleep(5)
+def findArduinoSerialNumber(info):
+    serialBegin = info.index(ARDUINO_SERIAL_NUMBER_IDENTIFIER) + len(ARDUINO_SERIAL_NUMBER_IDENTIFIER)
+    serialEnd = serialBegin + ARUDINO_SERIAL_NUMBER_LENGTH
 
+    return info[serialBegin:serialEnd]
+
+def findArduinos():
+    portList = list(serial.tools.list_ports.comports())
+
+    for port in portList:
+        isArduino = True
+        pidIndex = 0
+        portIndex = 0
+
+        # detect where the serial number is located
+        if ARDUINO_UNO_PID in port[0]:
+            pidIndex = 0
+        elif ARDUINO_UNO_PID in port[1]:
+            pidIndex = 1
+        elif ARDUINO_UNO_PID in port[2]:
+            pidIndex = 2
+        else:
+            isArduino = False
+
+        if isArduino:
+            # detect where the serial port path is located
+            if SERIAL_PORT_PATH in port[0]:
+                portIndex = 0
+            elif SERIAL_PORT_PATH in port[1]:
+                portIndex = 1
+            elif SERIAL_PORT_PATH in port[2]:
+                portIndex = 2
+
+            serialPort = port[portIndex]
+            serialNumber = findArduinoSerialNumber(port[pidIndex])
+
+            # Check if Arduino is already in list
+            if not serialNumber in connectedArduinos:
+                print("New Arduino " + serialNumber + " on port " + serialPort)
+                device = serial.Serial(serialPort, 9600)
+                connectedArduinos[serialNumber] = device
+                arduinoStatus[serialNumber] = True
+                saveArduinoEvents(serialNumber, serialPort, 'CONNECT')
+            else:
+                device = connectedArduinos[serialNumber]
+
+                if device.port != serialPort:
+                    print("Reconnecting to Arduino " + serialNumber + " on port " + serialPort)
+                    device.close()
+                    device.setPort(serialPort)
+                    device.open()
+                    arduinoStatus[serialNumber] = True
+                    saveArduinoEvents(serialNumber, serialPort, 'RECONNECT')
+                    
 while True:
-    data = ser.readline().split()
-    print("INPUT: " + str(len(data)))
-    if len(data) == DATA_LENGTH:
-        print("Saving data...")
-        saveToDatabase(data)
+    findArduinos()
+
+    #print("Connected Arudinos: " + str(len(connectedArduinos)))
+
+    for deviceID in connectedArduinos:
+        device = connectedArduinos[deviceID]
+        try:
+            #print("Checking input on " + deviceID);
+            if device.inWaiting() != 0:
+                # Read and print raw data
+                rawData = device.readline(500)
+                print(rawData)
+
+                # Check for Arduino reset status message
+                if "RESET ARDUINO" in rawData:
+                    saveArduinoEvents(deviceID, device.port, 'DAILY_RESET')
+
+                # If experiment data, store them
+                data = rawData.split()
+                if len(data) == DATA_LENGTH:
+                    print("Saving data...")
+                    saveToDatabase(data)
+        except:
+            if arduinoStatus[deviceID] == True:
+                arduinoStatus[deviceID] = False
+                print("Arduino " + deviceID + " on port " + device.port + " disconnected!")
+                saveArduinoEvents(deviceID, device.port, 'DISCONNECT')
+            pass
+
